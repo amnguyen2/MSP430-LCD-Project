6b56aba15bfa5a0114f8dbc84cb2b97822e4b90a
amnguyen2 <abramnguyen210@gmail.com>
2020-07-27 22:18:48 -0600

    fixed minor bug

--- project/stateMachines.h
+++ project/stateMachines.h
@@ -5,4 +5,5 @@ void sm_update_led(); // update led values
 void sm_update_buzzer(); // update buzzer sound
 void sm_update_lcd(); // update lcd screen
 
+void state_advance(unsigned int in); 
 #endif
