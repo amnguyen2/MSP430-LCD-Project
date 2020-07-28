53870b6560127912bbe693e891590432c13e97c5
amnguyen2 <abramnguyen210@gmail.com>
2020-07-27 22:47:27 -0600

    added toggle red assembly function

--- project/stateMachines.h
+++ project/stateMachines.h
@@ -1,6 +1,7 @@
 #ifndef stateMachine_included
 #define stateMachine_included
 
+char toggle_red(); // toggle red led on/off
 void sm_update_led(); // update led values
 void sm_update_buzzer(); // update buzzer sound
 void sm_update_lcd(); // update lcd screen
